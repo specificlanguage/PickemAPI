d4d56fd6ae9caab5e505bd2b318c153fb88166b6
specificlanguage <specificlanguage@gmail.com>
2024-01-24 14:15:43 -0500

    minor: Add 127.0.0.1 as an allowed origin

--- pickem/main.py
+++ pickem/main.py
@@ -17,7 +17,7 @@ app.include_router(picks.router)
 # TODO use env variables depending on env
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
