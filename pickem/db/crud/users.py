183a1eeedb3a112b7d94ece14752106e5f68dafb
specificlanguage <specificlanguage@gmail.com>
2024-04-10 19:18:07 -0400

    feat: Expanded profile page
    - py: Added new field for users for descriptions -- Just a description for the user to set, will create page on frontend to set this soon.
    - py: Retrieving a user also retrieves their preferences for ease of use
    - rct: Expanded profile page, top part includes a "business card"-like item that shows the user, their favorite team, etc.
    - rct: Adds loading wheels for long load times (which this often has)
    - rct: See earlier commit, refactored profile dropdown to move NavigationMenu to earlier component
    - rct: Modified getUser getter function to retrieve a user based on their uid or username

--- pickem/db/crud/users.py
+++ pickem/db/crud/users.py
@@ -10,13 +10,15 @@ def setUserPreferences(db: Session, preferences: schemas.UserPreferences):
         preferences.favoriteTeam = None
     userPrefs = models.User(id=preferences.id,
                             favoriteTeam_id=preferences.favoriteTeam,
-                            selectionTiming=preferences.selectionTiming)
+                            selectionTiming=preferences.selectionTiming,
+                            description=preferences.description)
     db.execute(text(f"""
         INSERT INTO users ("id", "favoriteTeam_id", "selectionTiming") 
         VALUES ('{userPrefs.id}', {userPrefs.favoriteTeam_id or 'NULL'}, '{userPrefs.selectionTiming}') 
         ON CONFLICT (id) DO UPDATE SET
         "favoriteTeam_id" = {userPrefs.favoriteTeam_id or 'NULL'},
         "selectionTiming" = '{userPrefs.selectionTiming}'
+        "description" = '{userPrefs.description}'
     """))
     db.commit()
 
